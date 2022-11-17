b38501ab430fe10a6870f63e05623d48ed66b4c5
akash <akashrp512@gmail.com>
2022-11-17 17:21:58 +0530

    commit

--- App.js
+++ App.js
@@ -8,15 +8,20 @@ const port = 3000;
 app.get("/", (req, res)=>{
      const url = "https://api.openweathermap.org/data/2.5/weather?q=bangalore&appid=f3698b699832a3ffb2b52c8fef2293e2&units=metric";
 
-    https.get(url, (response)=>{
-   console.log(response.statusCode);
-   response.on("data", (data)=>{
-    const weatherData = JSON.parse(data);
- const temp = weatherData.main.temp;
- console.log(temp)
+     https.get(url, (response)=>{
+     console.log(response.statusCode);
+     response.on("data", (data)=>{
+       const weatherData = JSON.parse(data);
+       const temp = weatherData.main.temp;
+       const weatherDescription = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const imageURL = ""
+        res.write("<p>The Weather currently" + weatherDescription +"<p>");
+        res.write("<h1>The temparature in Bengaluru is "+ temp + " degree Celsius.</h1>");
+        res.send()
    })
     })
-    res.send("Weather App")
+   
 });
 
 
